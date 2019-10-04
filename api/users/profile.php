cbe36796fda75fde0b6caa4bf08881690e0b1e99
shiro <repik2099@gmail.com>
2019-10-04 15:36:13 +0000

    users 04.10.2019 18:36

--- api/users/profile.php
+++ api/users/profile.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/protectedaut.php';
+require_once __DIR__ . '/../authentication/protectedaut.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
